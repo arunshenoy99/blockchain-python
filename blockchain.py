4d78beaaaa0161153d1ce9a87e52affc4dcaea7c
Arun Shenoy <devarunshenoy99@gmail.com>
2019-08-11 15:02:08 +0530

    init commit

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+blockchain = []
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+def add_value(transaction_amount, last_transaction = [1]):
+    blockchain.append([last_transaction,transaction_amount])
+
+add_value(2)
+add_value(5.7,get_last_blockchain_value())
+add_value(5.8,get_last_blockchain_value())
+
+print(blockchain)
---
+++ python-basics/functions.py
@@ -0,0 +1,3 @@
+def greeting(name , age):
+    print('Hello '+name+' your age is '+str(age))
+greeting(age=29, name = 'Arun')
